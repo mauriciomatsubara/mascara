64b6048df46f9ba2c8ab99de2e73fc7ae146550d
Mauricio Matsubara <mauricio.matsubara@gmail.com>
2020-04-29 22:11:20 -0300

    prd adj

--- scripts/url-staging-prod.sql
+++ scripts/url-staging-prod.sql
@@ -1,4 +1,4 @@
-UPDATE wp_options SET option_value = replace(option_value, 'https://mascara.teste.pro.br', 'https://www.masklife.com.br') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_options SET option_value = replace(option_value, 'https://mascara.teste.pro.br', 'https://www.masklife.com.br') WHERE option_name = 'theme_mods_flatsome-child' OR 'home' OR option_name = 'siteurl';
 UPDATE wp_posts SET guid = replace(guid, 'https://mascara.teste.pro.br', 'https://www.masklife.com.br');
 UPDATE wp_posts SET post_content = replace(post_content, 'https://mascara.teste.pro.br', 'https://www.masklife.com.br');
 
