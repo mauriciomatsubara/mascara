96cc39ee45a2bca28d6d11cc6e5d6904a11ec136
Mauricio Matsubara <mauricio.matsubara@gmail.com>
2020-05-20 17:23:30 -0300

    I'm a fucking stupid

--- scripts/url-prod-staging.sql
+++ scripts/url-prod-staging.sql
@@ -1,4 +1,4 @@
-UPDATE wp_options SET option_value = replace(option_value, 'https://www.masklife.com.br', 'https://mascara.teste.pro.br') WHERE option_name = 'theme_mods_flatsome-child' OR 'home' OR option_name = 'siteurl';
+UPDATE wp_options SET option_value = replace(option_value, 'https://www.masklife.com.br', 'https://mascara.teste.pro.br') WHERE option_name = 'theme_mods_flatsome-child' OR option_name = 'home' OR option_name = 'siteurl';
 UPDATE wp_posts SET guid = replace(guid, 'https://www.masklife.com.br', 'https://mascara.teste.pro.br');
 UPDATE wp_posts SET post_content = replace(post_content, 'https://www.masklife.com.br', 'https://mascara.teste.pro.br');
 
